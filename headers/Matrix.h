a0a420367ab6a1be930c4b7a994c01293b178a09
DerekF <derekforgione@gmail.com>
2011-10-20 01:48:58 -0400

    Update Matrix.h"
    "
    vcvc

--- headers/Matrix.h
+++ headers/Matrix.h
@@ -11,7 +11,16 @@ struct matrix
 	m41,m42,m43,m44;
 };
 
-void SetMatrixTranslation(matrix & m, const vector3 & translation);
+void SetMatrixTranslation(matrix & m, const vector3 & value);
+void SetMatrixUp(matrix & m, const vector3 & value);
+void SetMatrixRight(matrix & m, const vector3 & value);
+void SetMatrixBackward(matrix & m, const vector3 & value);
+
+void GetMatrixTranslation(const matrix & m, vector3 & result);
+void GetMatrixUp(const matrix & m, vector3 & result);
+void GetMatrixRight(const matrix & m, vector3 & result);
+void GetMatrixBackward(const matrix & m, vector3 & result);
+
 void InvertMatrix(const matrix & input, matrix & result);
 void MulMatrix(const matrix & a,const matrix & b, matrix & result);
 void ScaleMatrix(const matrix & input, const float & scalar, matrix & result);
